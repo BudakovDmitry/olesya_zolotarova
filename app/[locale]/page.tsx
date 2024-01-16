f9447da7013b636ced2006592ef67ced823e174a
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-01-17 01:15:49 +0200

    Add styles, variables. And add header component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,15 +2,32 @@ import {useTranslations} from 'next-intl';
 import {unstable_setRequestLocale, getTranslations} from 'next-intl/server';
 import LanguageSwitcher from "@/app/components/LanguageSwitcher";
 
+import '../styles/main.scss'
+
 
 const Home = ({params: {locale}}: any) => {
     unstable_setRequestLocale(locale);
     const t = useTranslations('Home');
+    const languages = ['uk', 'ru']
+
     return (
-        <div className='container'>
-            <LanguageSwitcher />
-            <h1>{t('title')}</h1>
-        </div>);
+        <>
+            <header className='header'>
+                <div className='container header__container'>
+                    <div className="header__logo"><a className='header__logo_link' href="#">Олеся Золотарева</a></div>
+                    <nav className='header__nav nav'>
+                        <ul className='nav__list'>
+                            <li className='nav__list_item'><a className='nav__list_link' href="#">Обо мне</a></li>
+                            <li className='nav__list_item'><a className='nav__list_link' href="#">Про интенсив</a></li>
+                            <li className='nav__list_item'><a className='nav__list_link' href="#">Тарифы</a></li>
+                            <li className='nav__list_item'><a className='nav__list_link' href="#">Контакты</a></li>
+                        </ul>
+                    </nav>
+                    <LanguageSwitcher options={languages} />
+                </div>
+            </header>
+        </>
+    );
 }
 
 export default Home
--- app/components/LanguageSwitcher.tsx
+++ app/components/LanguageSwitcher.tsx
@@ -1,11 +1,34 @@
+'use client'
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 
-const LanguageSwitcher = () => {
+const LanguageSwitcher = ({ options, onSelect }: any) => {
+    const currentPath = window.location.pathname
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOptionClick = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <div>
-            <Link href="/uk">Українська</Link>
-            {' | '}
-            <Link href="/ru">Русский</Link>
+        <div className="dropdown-container">
+            <button className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
+                <span>{currentPath.substring(1) || 'uk'}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M6 9L12 15L18 9" stroke="#496E0D" strokeWidth="3" strokeLinecap="round"
+                          strokeLinejoin="round"/>
+                </svg>
+            </button>
+            <ul className={`dropdown-list ${isOpen ? 'open' : ''}`}>
+                {options?.map((option: any) => (
+                    <li className="dropdown-item" key={option}>
+                        <a href={`/${option.toLowerCase()}`} className='dropdown-item_link'  onClick={() => handleOptionClick()}>
+                            {option}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
