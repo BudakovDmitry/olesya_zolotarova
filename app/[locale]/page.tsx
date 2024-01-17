edb568c12231c46367cd4391e837813319086532
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-01-17 23:58:30 +0200

    Add statistics section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -44,6 +44,24 @@ const Home = ({params: {locale}}: any) => {
                         </div>
                     </div>
                 </section>
+                <section className='statistics_section'>
+                    <div className="container">
+                        <div className="statistics_section__container">
+                            <div className="statistics_section__item">
+                                <div className="statistics_section__item_count">450+</div>
+                                <p className="statistics_section__item_text">довольных клиентов</p>
+                            </div>
+                            <div className="statistics_section__item">
+                                <div className="statistics_section__item_count">5</div>
+                                <p className="statistics_section__item_text">лет опыта </p>
+                            </div>
+                            <div className="statistics_section__item">
+                                <div className="statistics_section__item_count">2000+</div>
+                                <p className="statistics_section__item_text">часов консультаций </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     );
