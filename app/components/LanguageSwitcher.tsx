fc9c381c633b17f2929ecd0c9f7c5c1e30f64814
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-01-27 00:09:55 +0200

    Update Language Switcher

--- app/components/LanguageSwitcher.tsx
+++ app/components/LanguageSwitcher.tsx
@@ -1,12 +1,11 @@
 'use client'
 
 import React, { useState } from 'react';
-import Link from 'next/link';
 
-const LanguageSwitcher = () => {
-    // const currentPath = window.location.pathname
+const LanguageSwitcher = ({locale}: any) => {
     const [isOpen, setIsOpen] = useState(false);
     const languages = ['uk', 'ru']
+
 
     const handleOptionClick = (e: any) => {
         e.preventDefault();
@@ -16,8 +15,7 @@ const LanguageSwitcher = () => {
     return (
         <div className="dropdown-container">
             <button className="dropdown-button" onClick={handleOptionClick}>
-                {/*<span>{currentPath.substring(1) || 'uk'}</span>*/}
-                <span>uk</span>
+                <span>{locale || 'uk'}</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M6 9L12 15L18 9" stroke="#496E0D" strokeWidth="3" strokeLinecap="round"
                           strokeLinejoin="round"/>
