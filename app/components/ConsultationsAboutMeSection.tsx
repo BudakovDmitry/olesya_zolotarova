ce93c777344f4e24d1f0a24e1e4341422d67890c
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-02-03 20:03:39 +0200

    Update all section for consultations page mobile

--- app/components/ConsultationsAboutMeSection.tsx
+++ app/components/ConsultationsAboutMeSection.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+import ConsultationsMainPhoto from "@/public/consultations_main_photo.jpg";
+
 const ConsultationsAboutMeSection = () => {
     return (
         <section className='consultations_about_me_section'>
             <div className="consultations_about_me_section__container container">
-                <div className="consultations_about_me_section__photo_container"></div>
+                <div className="consultations_about_me_section__photo_container">
+                    <Image src={ConsultationsMainPhoto} alt="Main photo" className='consultations_about_me_section__photo' priority={true} unoptimized/>
+                </div>
                 <div className="consultations_about_me_section__content">
                     <p className="consultations_about_me_section__text">Окончила институт интегральной
                         превентивной и антивозрастной
--- app/components/ConsultationsListSection.tsx
+++ app/components/ConsultationsListSection.tsx
@@ -51,73 +51,73 @@ const ConsultationsListSection = () => {
                     {/*           className='consultations_list_section__list_item_icon' unoptimized/>*/}
                     {/*</li>*/}
                 </ul>
-                {/*<ul className="consultations_list_section__list">*/}
-                {/*    <li className="consultations_list_section__list_item one">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Лишний вес</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item two">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Нарушения работы ЖКТ (кандидоз, дисбактериоз, СИБР, СИГР, застой желчи, язвы, эррозии, рефлюксы)</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item three">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Нарушение работы щитовидной железы</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item four">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Постковид</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item five">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Переедание, тяга к вредным продуктам</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item six">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Апатия, хроническая усталость, сонливость (СУН, митохондриальная дисфункция, хроническое воспаление)</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item seven">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Проблемы с концентрацией внимания, плохая память</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item eight">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Высыпания, акне, выпадение волос</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item nine">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Подготовка к зачатию</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item ten">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Послеродовое восстановление</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item eleven">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Низкое либидо</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item twelve">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Плаксивость, нервозность</span>*/}
-                {/*    </li>*/}
-                {/*    <li className="consultations_list_section__list_item thirteen">*/}
-                {/*        <Image src={ListMarker} alt="List marker"*/}
-                {/*               className='consultations_list_section__list_item_icon' unoptimized/>*/}
-                {/*        <span className='consultations_list_section__list_item_text'>Плаксивость, нервозность</span>*/}
-                {/*    </li>*/}
-                {/*</ul>*/}
+                <ul className="consultations_list_section__list_mobile">
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Лишний вес</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Нарушения работы ЖКТ (кандидоз, дисбактериоз, СИБР, СИГР, застой желчи, язвы, эррозии, рефлюксы)</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Нарушение работы щитовидной железы</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Постковид</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Переедание, тяга к вредным продуктам</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Апатия, хроническая усталость, сонливость (СУН, митохондриальная дисфункция, хроническое воспаление)</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Проблемы с концентрацией внимания, плохая память</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Высыпания, акне, выпадение волос</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Подготовка к зачатию</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Послеродовое восстановление</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Низкое либидо</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Плаксивость, нервозность</span>
+                    </li>
+                    <li className="consultations_list_section__list_item">
+                        <Image src={ListMarker} alt="List marker"
+                               className='consultations_list_section__list_item_icon' unoptimized/>
+                        <span className='consultations_list_section__list_item_text'>Плаксивость, нервозность</span>
+                    </li>
+                </ul>
             </div>
         </section>
     )
