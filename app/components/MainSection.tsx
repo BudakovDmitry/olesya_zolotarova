0decde69d41ded57a3ff11bbb05f9b12f412e845
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-02-07 15:28:53 +0200

    Fix 7.02

--- app/components/MainSection.tsx
+++ app/components/MainSection.tsx
@@ -3,7 +3,7 @@
 import {motion} from "framer-motion"
 import Image from "next/image";
 import MainPhoto from "@/public/main_photo.jpg";
-import MainPhotoMobile from "@/public/main_photo_mobile_new.jpg";
+import MainPhotoMobile from "@/public/main_photo_mobile_new.webp";
 
 const MainSection = () => {
     return (
@@ -15,23 +15,31 @@ const MainSection = () => {
         >
             <section className='main_section'>
                 <Image src={MainPhoto} alt="Main photo" className='main_section__photo' priority={true} unoptimized/>
-                <Image src={MainPhotoMobile} alt="Main photo" className='main_section__photo_mobile' priority={true} unoptimized/>
-                <div className="main_section__content container">
-                    <div className="main_section__content_container">
-                        <h2 className='main_section__subtitle'>Интенсив</h2>
-                        <h1 className='main_section__title'>ПЕРЕЗАГРУЗКА</h1>
-                        <p className='main_section__description'>Перезагрузите свои привычки и питание со мной всего
-                            за 10 дней</p>
-                        <div className="main_section__button_container">
-                            <a href="#tariffs" className='btn btn__primary main_section__button'>Учавствовать</a>
-                            <a href="#about_intensive"
-                               className='btn btn__secondary main_section__button main_section__button_more'>подробнее</a>
+                <motion.div
+                    initial={{opacity: 0}}
+                    whileInView={{opacity: 1}}
+                    transition={{ease: "easeOut", duration: 1}}
+                    viewport={{once: true}}
+                >
+                    <Image src={MainPhotoMobile} alt="Main photo" className='main_section__photo_mobile' priority={true}
+                           unoptimized/>
+                </motion.div>
+                    <div className="main_section__content container">
+                        <div className="main_section__content_container">
+                            <h2 className='main_section__subtitle'>Интенсив</h2>
+                            <h1 className='main_section__title'>ПЕРЕЗАГРУЗКА</h1>
+                            <p className='main_section__description'>Перезагрузите свои привычки и питание со мной всего
+                                за 10 дней</p>
+                            <div className="main_section__button_container">
+                                <a href="#tariffs" className='btn btn__primary main_section__button'>Учавствовать</a>
+                                <a href="#about_intensive"
+                                   className='btn btn__secondary main_section__button main_section__button_more'>подробнее</a>
+                            </div>
                         </div>
                     </div>
-                </div>
             </section>
         </motion.div>
-    )
+)
 }
 
 export default MainSection;
