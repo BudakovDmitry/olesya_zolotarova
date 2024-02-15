3a04881693f4d56cae1f06377a8a77c77cac4261
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-02-15 13:17:24 +0200

    Fix 7.02

--- app/components/ConsultationsFooter.tsx
+++ app/components/ConsultationsFooter.tsx
@@ -1,7 +1,7 @@
 const ConsultationsFooter = () => {
     return (
         <footer className="consultations_footer">
-            <a href="https://budakovdmitry.github.io/my-site-portfolio" className="consultations_footer__link">Created by Budakov Dmytro</a>
+            <a href="https://budakovdmitry.github.io/my-site-portfolio" target='_blank' className="consultations_footer__link">Created by Budakov Dmytro</a>
         </footer>
     )
 }
